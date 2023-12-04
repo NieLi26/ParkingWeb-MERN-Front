255884f72730b25a09a81100ca07811a5a6b259c
NieLi26 <se.olivaresc@gmail.com>
2023-12-04 15:29:56 -0300

    front crud lote, tarifa, manejo de reserva

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -109,11 +109,17 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen, classNames }) => {
         {/* Static sidebar for desktop */}
         <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
           {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex flex-col flex-grow border-r border-gray-200 pt-5 bg-white overflow-y-auto">
+          <div className="flex flex-col flex-grow border-r border-gray-200 pt-2 bg-white overflow-y-auto">
             <div className="flex items-center flex-shrink-0 px-4">
-              <img
+            {/* <div className="flex items-center flex-shrink-0 px-4"> */}
+              {/* <img
                 className="h-8 w-auto"
                 src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
+                alt="Workflow"
+              /> */}
+              <img
+                className="h-12 w-auto"
+                src="logo.ico"
                 alt="Workflow"
               />
             </div>
